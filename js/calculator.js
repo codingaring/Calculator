cfe4aacc665a460c4e36742ec7d059a6d70a70f9
codingaring <sksms2104@naver.com>
2024-02-02 12:17:04 +0900

    [Feat] 다크모드 전환 기능 추가

--- js/calculator.js
+++ js/calculator.js
@@ -1,6 +1,9 @@
 const display = document.querySelector("#display");
 const buttons = document.querySelector("#buttons");
 const modeChangeBtn = document.querySelector("#mode-change-btn");
+const calculatorSection = document.querySelector("#calculator-section");
+const button = document.getElementsByTagName("button");
+const toggleList = [calculatorSection, button, display];
 
 let currentInput = "";
 
@@ -27,6 +30,26 @@ function handleButtonClick(input) {
   display.textContent = currentInput;
 }
 
+// 입력값 display에 띄우는 이벤트 등록
 buttons.addEventListener("click", function (event) {
   handleButtonClick(event.target.textContent);
 });
+
+// 아이콘 클릭하면 전환 (다크모드 설정)
+modeChangeBtn.addEventListener("click", function () {
+  modeChangeBtn.getAttribute("src") === "./imgs/dark-mode-icon.png"
+    ? (modeChangeBtn.setAttribute("src", "./imgs/bright-mode-icon-white.png"),
+      modeChangeBtn.setAttribute("alt", "기본 모드"),
+      toggleList.forEach(ModeTodark))
+    : (modeChangeBtn.setAttribute("src", "./imgs/dark-mode-icon.png"),
+      modeChangeBtn.setAttribute("alt", "다크 모드"),
+      toggleList.forEach(ModeToBright));
+});
+
+function ModeTodark(list) {
+  list.classList.add("dark");
+}
+
+function ModeToBright(list) {
+  list.classList.remove("dark");
+}
