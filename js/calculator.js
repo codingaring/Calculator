0ca568996fba197cca69dd20a908a43a19d81a3b
codingaring <sksms2104@naver.com>
2024-02-02 11:48:41 +0900

    [Feat] 버튼 클릭시, 디스플레이에 입력값 보여주는 기능 추가

--- js/calculator.js
+++ js/calculator.js
@@ -1,1 +1,32 @@
-import { querySelector } from "./utils/querySelector.js";
+const display = document.querySelector("#display");
+const buttons = document.querySelector("#buttons");
+const modeChangeBtn = document.querySelector("#mode-change-btn");
+
+let currentInput = "";
+
+// 입력값을 display에 띄우는 함수
+function handleButtonClick(input) {
+  switch (input) {
+    case "=":
+      try {
+        const result = new Function(
+          "return " + currentInput.replace(/x/g, "*")
+        )();
+        currentInput = result.toString();
+      } catch (error) {
+        console.log(error);
+        currentInput = "에러";
+      }
+      break;
+    case "AC":
+      currentInput = "";
+      break;
+    default:
+      currentInput += input;
+  }
+  display.textContent = currentInput;
+}
+
+buttons.addEventListener("click", function (event) {
+  handleButtonClick(event.target.textContent);
+});
